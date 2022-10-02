6a14f6d65897e9482f275e2def3ab4f5a49a3ab6
alex <alexshaoyu@gmail.com>
2022-10-02 17:46:09 +0800

    Initial Commit

---
+++ homework1/homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  homework1App.swift
+//  homework1
+//
+//  Created by 沈劭羽 on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
